c5edf755978b9ea7254b51bc16a821d851812cf8
Nuunnzzz <noha_malek@outlook.com>
2020-12-13 23:02:54 +0200

    deleted tabs component

--- whatisnext/src/components/explore/explore.js
+++ whatisnext/src/components/explore/explore.js
@@ -1,9 +1,12 @@
 import CareerCard from "../career-card/career-card";
 import './explore.css';
+import SecondNav from '../secondNav/second-nav.js'
 
 const Explore = () => {
     return(
-        <div className="careers-container">
+        <div>
+            <SecondNav/>
+                    <div className="careers-container">
             <h2>Explore Careers</h2>
             <div className="cards" id='explore-career'>
                 <CareerCard />
@@ -14,6 +17,7 @@ const Explore = () => {
                 <CareerCard />
             </div>
 
+        </div>
         </div>
         
     );
